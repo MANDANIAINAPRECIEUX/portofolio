672a9ceda9947e17ca12e2ba752c64ea01687302
unknown <mandaniainaprecieux@yahoo.com>
2026-01-24 23:56:53 +0300

    debut Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Navbar from "./components/Navbar";
 
 function App() {
-  
-
   return (
     <>
+      <Navbar />
       <div>bonjour</div>
-      <div><span> je suis </span><span>ANDRIAMIARIONS M. Précieux</span></div>
-      
+      <div>
+        <span> je suis </span>
+        <span>ANDRIAMIARIONS M. Précieux</span>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
