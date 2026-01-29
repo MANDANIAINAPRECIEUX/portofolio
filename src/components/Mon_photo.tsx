c854a8d00186ee3846ef23ff349eb97765d7b32d
unknown <mandaniainaprecieux@yahoo.com>
2026-01-29 23:38:09 +0300

    mod photo

--- src/components/Mon_photo.tsx
+++ src/components/Mon_photo.tsx
@@ -5,8 +5,8 @@ const Mon_photo = () => {
         <img
           src="/photos/manda.png"
           alt="Photo de profil"
-          className="w-full h-full object-cover   drop-shadow-[0_0_30px_rgba(255,255,255,0.9)] 
-             transition-all duration-300 ease-out
+          className="w-full h-full object-cover   drop-shadow-[0_0_20px_rgba(255,255,255,0.9)] 
+          transition-all duration-300 ease-out
             hover:scale-105
             hover:drop-shadow-[0_0_40px_rgba(255,255,255,1)] "
         />
